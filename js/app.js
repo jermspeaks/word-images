890accb7091be7e87f15aa0a711522773b57e2e1
jermspeaks <jermspeaks@gmail.com>
2014-10-15 13:15:38 -0700

    get request url

--- js/app.js
+++ js/app.js
@@ -1,1 +1,33 @@
 var app = angular.module('wordImages', []);
+
+app.factory('words', ['$http', function($http){
+  var o = {
+    phrase: ''
+  };
+
+  o.encoded = function(){
+    return encodeURIComponent(o.phrase);
+  };
+  return o;
+}]);
+
+app.controller('SearchCtrl', ['$scope', '$http', 'words', function($scope, $http, words){
+  $scope.words = words;
+
+  $scope.searchTerm = function(){
+    $scope.words.phrase = $scope.query;
+    $scope.url = "http://api.wordnik.com:80/v4/word.json/" + $scope.words.encoded() + "?useCanonical=false&includeSuggestions=false&api_key=" + apiKey;
+    $http.get($scope.url)
+      .success(function(){
+        console.log('success');
+      })
+      .error(function(){
+        console.log('failure');
+      });
+    // Create the http get request to wordnik
+    // the data holds the search term
+    // The request is a JSON request.
+
+
+  };
+}]);
